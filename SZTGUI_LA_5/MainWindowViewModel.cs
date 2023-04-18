5150a341141aae6c280dc58195a1dad4a10f2f52
dlehanykornelia8@stud.uni-obuda.hu <dlehanykornelia8@stud.uni-obuda.hu>
2023-04-18 08:49:57 +0200

    ViewModel created

---
+++ SZTGUI_LA_5/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTGUI_LA_5
+{
+    class MainWindowViewModel
+    {
+    }
+}
